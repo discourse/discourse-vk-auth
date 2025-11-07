13b89fd50eb75fbe0b76476fd4d1416a04480c9e
Penar Musaraj <pmusaraj@gmail.com>
2025-11-07 10:58:41 -0500

    bump omniauth-vkontakte to 1.9.0

--- plugin.rb
+++ plugin.rb
@@ -11,7 +11,7 @@ enabled_site_setting :vk_auth_enabled
 enabled_site_setting :vk_app_id
 enabled_site_setting :vk_secure_key
 
-gem "omniauth-vkontakte", "1.7.1"
+gem "omniauth-vkontakte", "1.9.0"
 
 class Auth::VkontakteAuthenticator < Auth::ManagedAuthenticator
   def name
